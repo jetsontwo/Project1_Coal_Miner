e0aa59cfae1ab8f79c76c96d4c3b27d53a6566c4
jetsontwo <jetsontwo@gmail.com>
2016-05-04 15:33:30 -0700

    Attached camera to player and added side walls to platforming section

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class Player_Movement : MonoBehaviour {
 
     public float spd, jump_height;
+    public Camera c;
     private float move_Horiz, move_Vert, speed_mult;
+    private bool grounded;
     private Rigidbody rb;
+    private Transform c_loc;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
+        grounded = false;
+        c_loc = c.transform;
 	}
 	
 	// Update is called once per frame
@@ -22,10 +27,26 @@ public class Player_Movement : MonoBehaviour {
         {
             rb.AddForce(new Vector3(move_Horiz * spd, 0, move_Vert * spd));
         }
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             rb.AddForce(new Vector3(0f, jump_height, 0f));
         }
         transform.rotation.Set(0f, 0f, 0f, 0f);
+    }
+
+    public bool isGrounded()
+    {
+        return grounded;
+    }
+
+    void OnCollisionStay(Collision c)
+    {
+        if (c.gameObject.tag == "Ground"){
+            grounded = true;
+        }
+    }
+    void OnCollisionExit(Collision c)
+    {
+        grounded = false;
     }
 }
---
+++ Assets/Scripts/Wall_Push.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall_Push : MonoBehaviour {
+
+    public GameObject player;
+    private Player_Movement pm;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = player.GetComponent<Rigidbody>();
+        pm = player.GetComponent<Player_Movement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void OnCollisionStay(Collision c)
+    {
+        if (!pm.isGrounded()){
+            rb.velocity = new Vector3(0f, -4f, 0f);
+        }
+    }
+}
