a8fa04aa7b55a0afb418f666823ddc8a8dd8bf3c
jetsontwo <jetsontwo@gmail.com>
2016-05-11 11:53:55 -0700

    Added rotation based of mouse movement and lockes cursor in middle of screen, also added pause feature using p

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -4,12 +4,14 @@ using System.Collections;
 public class Player_Movement : MonoBehaviour {
 
     public float spd, jump_height;
+    public float turn_speed;
     public Camera c;
     private float move_Horiz, move_Vert, speed_mult;
-    private bool grounded;
+    private bool grounded, tracking;
     private Rigidbody rb;
     private Transform c_loc;
     public int coal_count;
+    private Vector3 mouse_past, mouse_current;
 
 	// Use this for initialization
 	void Start () {
@@ -17,20 +19,51 @@ public class Player_Movement : MonoBehaviour {
         grounded = false;
         c_loc = c.transform;
         coal_count = 0;
+        mouse_past = Input.mousePosition;
+        mouse_current = Input.mousePosition;
+        Cursor.lockState = CursorLockMode.Locked;
+        tracking = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        mouse_current = Input.mousePosition;
         move_Horiz = -Input.GetAxis("Horizontal");
         move_Vert = -Input.GetAxis("Vertical");
         if (rb.velocity.magnitude < 10)
         {
-            rb.AddForce(new Vector3(move_Horiz * spd, 0, move_Vert * spd));
+            print(transform.forward);
+            rb.AddForce(new Vector3(move_Horiz * spd * transform.forward.x, 0, move_Vert * spd * transform.forward.z));
         }
         if (Input.GetKeyDown(KeyCode.Space) && grounded)
         {
             rb.AddForce(new Vector3(0f, jump_height, 0f));
         }
+
+        if (tracking)
+        {
+            if (Input.GetAxis("Mouse X") > 0)
+                transform.Rotate(Vector3.forward * turn_speed);
+            if (Input.GetAxis("Mouse X") < 0)
+                transform.Rotate(Vector3.forward * -turn_speed);
+        }
+        
+
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            if (Cursor.lockState.Equals(CursorLockMode.Locked))
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                tracking = false;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                tracking = true;
+            }
+        }
+        
     }
 
     public bool isGrounded()
